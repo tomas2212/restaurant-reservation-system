93c54110d00dd3afe51cf31c91f4c507a22ae196
rzahradnicek@gmail.com <rzahradnicek@gmail.com@1e5a7547-d66b-575b-f771-38a02b2f8fff>
2011-06-23 09:27:19 +0000

    booking table printing not finished

--- trunk/Restaurant/src/java/cz/muni/fi/pb138/restaurant/Manager.java
+++ trunk/Restaurant/src/java/cz/muni/fi/pb138/restaurant/Manager.java
@@ -12,8 +12,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
